1133a452f65e1326aa7a01e221bf7b166496a605
xndrbrgs <aborgescolon@gmail.com>
2022-02-03 17:47:47 -0500

    adding db queries

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ function mainMenu() {
     inquirer
     .prompt([
         {
+            name: "choice",
             type: 'list',
-            name: "action",
             message: "MAIN MENU",
             choices: [
                 "View all departments",
@@ -32,12 +32,69 @@ function mainMenu() {
                 "View all employees",
                 "Add a department",
                 "Add a role",
-                "Add an emplotte",
+                "Add an employee",
                 "Update an employee's role"
             ]
         }
     ])
     .then((answer) => {
+        if (answer === "View all departments") {
+            returnDepartments();
+        }
+        
+        if (answer === "View all roles") {
+            returnRoles();
+        }
 
+        if (answer === "View all employees") {
+            returnEmployees();
+        }
+
+        if (answer === "Add a department") {
+            returnNewDepartment();
+        }
+
+        if (answer === "Add a role") {
+            returnNewRole();
+        }
+
+        if (answer === "Add an employee") {
+            returnNewEmployee();
+        }
+
+        if (answer === "Update an employee's role") {
+            updateEmpRole();
+        }
+
+        if (answer === "No Action") {
+            db.end();
+        };
+    });
+};
+
+// Function to return departments 
+
+returnDepartments = () => {
+    db.query("SELECT employees.first_name, employees.last_name, department.name AS Department FROM employees JOIN role ON employees.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employees.id;", 
+    (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
     })
-}
+};
+
+// Function to return roles 
+
+returnRoles = () => {
+    db.query("SELECT employees.first_name, employees.last_name, role.title AS Title FROM employees JOIN role ON employees.role_id = role.id", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    })
+};
+
+// Function to return all employees 
+
+
+
+
